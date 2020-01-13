63368a0a4204cedbe76dd86ed8ce2338c04d09a1
Nonni Linux <jglorange@gmail>
2020-01-13 21:36:35 +0000

    header fix?

--- src/components/GlobalStyle/index.js
+++ src/components/GlobalStyle/index.js
@@ -1,21 +1,6 @@
 import { createGlobalStyle } from "styled-components"
 
-/** fonts */
-import MaisonBook from "../../../static/assets/fonts/MaisonNeue-Book.otf"
-import MaisonBold from "../../../static/assets/fonts/MaisonNeue-Bold.otf"
-
 export const GlobalStyle = createGlobalStyle`
-    @font-face {
-        font-family: Maison Neue Book;
-        src: url(${MaisonBook});
-    }
-    @font-face {
-        font-family: Maison Neue Bd;
-        src: url(${MaisonBold});
-    }
-    * {
-        font-family: Maison Neue Book;
-    }
     html, body {
         margin: 0;
         height: 100%;
