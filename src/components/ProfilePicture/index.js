0a7584f3bd3b798562e4caab63a7c8a754c88cd4
Nonni Linux <jglorange@gmail>
2020-01-28 17:11:07 +0000

    pic

---
+++ src/components/ProfilePicture/index.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+/* components */
+import { Container } from "./Styled"
+
+const ProfilePicture = () => {
+  return <Container>ProfilePicture</Container>
+}
+
+export default ProfilePicture
