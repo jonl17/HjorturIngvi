83fa8d4b34bae7b297b637a260a920f148631a17
Nonni Linux <jglorange@gmail>
2020-02-14 11:57:55 +0000

    coming soon | projects

--- src/components/ProjectsComponent/index.js
+++ src/components/ProjectsComponent/index.js
@@ -9,18 +9,20 @@ import { useSelector } from "react-redux"
 const ProjectsComponent = ({ projects }) => {
   const icelandic = useSelector(state => state.reducer.icelandic)
 
-  return projects.map((project, index) => (
-    <Text key={index}>
-      <Anchor
-        className="bold"
-        to={"/projects/" + slugify(project.frontmatter.title)}
-      >
-        {!icelandic && project.frontmatter.title_en
-          ? project.frontmatter.title_en
-          : project.frontmatter.title}
-      </Anchor>
-    </Text>
-  ))
+  if (projects.length > 1) {
+    return projects.map((project, index) => (
+      <Text key={index}>
+        <Anchor
+          className="bold"
+          to={"/projects/" + slugify(project.frontmatter.title)}
+        >
+          {!icelandic && project.frontmatter.title_en
+            ? project.frontmatter.title_en
+            : project.frontmatter.title}
+        </Anchor>
+      </Text>
+    ))
+  } else return <h1>Coming soon...</h1>
 }
 
 export default ProjectsComponent
